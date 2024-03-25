7d4aa6699810fa7da6f1bc5dbc20b3129b2a654d
Mathias Private Laptop <mathiasberggren1@gmail.com>
2024-03-25 21:43:06 +0100

    Basic navigate through movies available and extract titles

--- apps/data-fetcher/src/index.ts
+++ apps/data-fetcher/src/index.ts
@@ -3,22 +3,28 @@ import puppeteer from 'puppeteer'
 (async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.justwatch.com/')
-    const movieTitles = await page.evaluate(() => {
-        const titles: string[] = []
-        document.querySelectorAll('.movie-title').forEach(title => {
-            titles.push(title.textContent || 'nothing found')
-        })
-        return titles
-    })
+    await page.goto('https://www.justwatch.com/se/filmer')
 
-    const data = await page.evaluate(() => {
-        const title = document?.querySelector('h1')?.innerText
-        return { title }
-    })
-    console.log('These are the movie titles', movieTitles)
-    console.log(data)
+    const linkSelector = 'a.title-list-grid__item--link';
+    const links = await page.$$eval(linkSelector, anchors => anchors.map(anchor => anchor.href));
+
+
+    let movies: string[] = [];
+
+
+    for (let href of links) {
+        await page.goto(href);
+
+        const info = await page.evaluate(() => {
+            return document.querySelector('h1')?.innerText;
+        });
+
+        info ? movies.push(info) : undefined;
+
+    }
+
+    console.log("This is the extracted info")
+    console.log(movies)
+
     await browser.close()
 })()
-
-console.log('Just testing')
