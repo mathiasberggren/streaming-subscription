280c898d37baadb73cd1be323643a51524b05740
rasouza <alves.wm@gmail.com>
2024-04-05 11:04:55 +0200

    package-based nx monorepo

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api-old',
+  coverageDirectory: '../../coverage/apps/api',
 };
