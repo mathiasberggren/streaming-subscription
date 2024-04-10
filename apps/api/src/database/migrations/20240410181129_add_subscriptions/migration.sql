ecdceeb480974e5d6fa31299bf79705d7d18b165
rasouza <alves.wm@gmail.com>
2024-04-10 20:13:31 +0200

    feat(api): :card_file_box: add migrations for subscription

---
+++ apps/api/src/database/migrations/20240410181129_add_subscriptions/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscriptions" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "company_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscription_credentials" (
+    "id" SERIAL NOT NULL,
+    "subscription_id" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "subscription_credentials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "subscriptions_user_id_company_id_idx" ON "subscriptions"("user_id", "company_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_user_id_company_id_key" ON "subscriptions"("user_id", "company_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscription_credentials_subscription_id_key" ON "subscription_credentials"("subscription_id");
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscription_credentials" ADD CONSTRAINT "subscription_credentials_subscription_id_fkey" FOREIGN KEY ("subscription_id") REFERENCES "subscriptions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
