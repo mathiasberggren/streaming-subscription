ded8840e508b2527662fe6a62cf7046d5a8a6a43
rasouza <alves.wm@gmail.com>
2024-04-10 21:10:09 +0200

    refactor(api): :card_file_box: add image field to company table

--- apps/api/src/database/migrations/20240410190859_add_subscriptions/migration.sql
+++ apps/api/src/database/migrations/20240410190859_add_subscriptions/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "companies" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
