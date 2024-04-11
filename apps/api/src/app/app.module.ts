15caf7c3179b112b523b122c46435fc9238d00a6
rasouza <alves.wm@gmail.com>
2024-04-11 21:29:14 +0200

    feat(api): :card_file_box: implement subscription service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { ZodValidationPipe } from 'nestjs-zod'
 
 import { DatabaseModule } from '../database/database.module'
 import { MoviesModule } from '../movies/movies.module'
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { validate } from './config/validate'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate }), DatabaseModule, MoviesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    DatabaseModule,
+    MoviesModule,
+    SubscriptionsModule
+  ],
   controllers: [AppController],
   providers: [
     Logger,
