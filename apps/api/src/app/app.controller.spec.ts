f594a5e3ababb2f32ce7d9701d46ceb641096fdd
rasouza <alves.wm@gmail.com>
2024-04-12 16:49:37 +0200

    test(api): :recycle: migrate existing AppController unit test to automock

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -1,40 +1,37 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { Test, TestingModule } from '@nestjs/testing'
 import { Request } from 'express'
 import { createMock } from '@golevelup/ts-jest'
 import { Logger } from '@nestjs/common'
+import { TestBed } from '@automock/jest'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let app: TestingModule
-  let appController: AppController
-  let logger: Logger
+  let controller: AppController
+  let logger: jest.Mocked<Logger>
+  let appService: jest.Mocked<AppService>
 
   const request = createMock<Request>()
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService, Logger]
-    })
-      .useMocker(createMock)
-      .compile()
+    const { unit, unitRef } = TestBed.create(AppController).compile()
 
-    appController = app.get<AppController>(AppController)
-    logger = app.get<Logger>(Logger)
+    controller = unit
+    appService = unitRef.get(AppService)
+    logger = unitRef.get(Logger)
   })
 
   describe('getData', () => {
     it('should return "Hello API"', () => {
-      expect(appController.getData(request)).toEqual({ message: 'Hello API' })
+      appService.getData.mockReturnValue({ message: 'Hello API' })
+
+      const response = controller.getData(request)
+      expect(response).toEqual({ message: 'Hello API' })
     })
 
     it('should log the request context', () => {
-      jest.spyOn(logger, 'log')
-
-      appController.getData(request)
+      controller.getData(request)
 
       expect(logger.log).toHaveBeenCalledWith(request, 'AppController#getData')
     })
