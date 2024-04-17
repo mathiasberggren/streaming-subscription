fe6b580512ed64b2405549f3ea1663ef72834786
rasouza <alves.wm@gmail.com>
2024-04-17 19:28:53 +0200

    build(api): add support for x64 and ARM architectures for building prisma client

--- apps/api/test/helpers/truncate-db.ts
+++ apps/api/test/helpers/truncate-db.ts
@@ -8,9 +8,9 @@ export default async () => {
   const tablenames = await prisma.$queryRaw<Array<{ tablename: string }>>`SELECT tablename FROM pg_tables WHERE schemaname='public'`
 
   const tables = tablenames
-    .map(({ tablename }) => tablename)
-    .filter((name) => name !== '_prisma_migrations')
-    .map((name) => `"public"."${name}"`)
+    .map(({ tablename }: { tablename: string }) => tablename)
+    .filter((name: string) => name !== '_prisma_migrations')
+    .map((name: string) => `"public"."${name}"`)
     .join(', ')
 
   try {
