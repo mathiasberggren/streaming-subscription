09597db056db8643bf82a3b4b0b64db52a9a362f
Mathias Private Laptop <mathiasberggren1@gmail.com>
2024-04-20 13:32:41 +0200

    refactor(api): :technologist: Update the movieTitle factory to demand a movieId to be passed in

--- apps/api/src/database/factories/movieTitle.ts
+++ apps/api/src/database/factories/movieTitle.ts
@@ -2,15 +2,19 @@ import { MovieTitle, PrismaClient } from '@prisma/client'
 import { Factory } from 'fishery'
 import { faker } from '@faker-js/faker'
 
+import { supportedLanguageLocaleCodes, Language } from '../../constants'
+
+import { generateMovieTitles } from './utils/generateMovieTitles'
+
 class MovieTitleFactory extends Factory<MovieTitle> {
 
 }
 
 export const movieTitleFactory = MovieTitleFactory.define(({ sequence, onCreate, params }) => {
-  if (params.movieId === undefined) {
-    throw new Error('movieId must be provided')
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!params.movieId) {
+    throw new Error('[movieTitleFactory] movieId must be provided')
   }
-
   onCreate(async (movieTitle) => {
     const prisma = new PrismaClient()
 
@@ -20,16 +24,12 @@ export const movieTitleFactory = MovieTitleFactory.define(({ sequence, onCreate,
     })
   })
 
-  const language = (params?.language !== undefined && params.language in wordLists)
-    ? params.language as keyof typeof wordLists
-    : faker.helpers.arrayElement(Object.keys(wordLists)) as keyof typeof wordLists
+  const language = (params?.language !== undefined && params.language in supportedLanguageLocaleCodes)
+    ? params.language as Language
+    : faker.helpers.arrayElement(supportedLanguageLocaleCodes)
 
-  const words = wordLists[language]
-
-  const adjective = faker.helpers.arrayElement(words.adjectives)
-  const noun = faker.helpers.arrayElement(words.nouns)
-  const verb = faker.helpers.arrayElement(words.verbs)
-  const title = `${adjective} ${noun} ${verb}`
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  const title = params.title ?? generateMovieTitles([language])[language]
 
   return {
     movieTitleId: sequence,
@@ -38,41 +38,3 @@ export const movieTitleFactory = MovieTitleFactory.define(({ sequence, onCreate,
     language
   }
 })
-
-const wordLists = {
-  en: {
-    adjectives: ['Quick', 'Bright', 'Dark'],
-    nouns: ['Fox', 'Star', 'Shadow'],
-    verbs: ['Jumps', 'Falls', 'Rises']
-  },
-  es: {
-    adjectives: ['Rápido', 'Brillante', 'Oscuro'],
-    nouns: ['Zorro', 'Estrella', 'Sombra'],
-    verbs: ['Salta', 'Cae', 'Asciende']
-  },
-  pt: {
-    adjectives: ['Rápido', 'Brilhante', 'Escuro'],
-    nouns: ['Raposa', 'Estrela', 'Sombra'],
-    verbs: ['Salta', 'Cai', 'Sobe']
-  },
-  se: {
-    adjectives: ['Snabb', 'Ljus', 'Mörk'],
-    nouns: ['Räv', 'Stjärna', 'Skugga'],
-    verbs: ['Hoppa', 'Fall', 'Stiger']
-  },
-  fr: {
-    adjectives: ['Rapide', 'Brillant', 'Obscur'],
-    nouns: ['Renard', 'Étoile', 'Ombre'],
-    verbs: ['Saute', 'Tombe', 'Monte']
-  },
-  de: {
-    adjectives: ['Schnell', 'Hell', 'Dunkel'],
-    nouns: ['Fuchs', 'Stern', 'Schatten'],
-    verbs: ['Springt', 'Fällt', 'Steigt']
-  },
-  it: {
-    adjectives: ['Veloce', 'Luminoso', 'Oscuro'],
-    nouns: ['Volpe', 'Stella', 'Ombra'],
-    verbs: ['Salta', 'Cade', 'Sale']
-  }
-} as const
