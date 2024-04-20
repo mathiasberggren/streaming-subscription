904dabaf3f093ea946e35948257614157f1de561
Mathias Private Laptop <mathiasberggren1@gmail.com>
2024-04-20 13:36:43 +0200

    feat(api): :sparkles: Use fuzzy match when searching through the DB

--- apps/api/src/movies/movies-search-db/movies-search-db.service.ts
+++ apps/api/src/movies/movies-search-db/movies-search-db.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common'
+import { Movie } from '@prisma/client'
 
 import { MoviesSearch } from '../interfaces/movies-search.interface'
 import { PrismaService } from '../../database/prisma.service'
 
 @Injectable()
 export class MoviesSearchDbService implements MoviesSearch {
-  constructor (private readonly db: PrismaService) {}
+  constructor (
+    private readonly db: PrismaService
+  ) {}
 
-  async findByTitle (title: string) {
-    return await this.db.movieTitle.findFirst({
-      where: { title },
-      include: { Movie: true }
-    })
+  async findByTitle (searchTitle: string, queryLimit: number = 10) {
+    const result = await this.db.$queryRaw<Movie[]>`
+      SELECT m.* FROM "Movie" m
+        JOIN "MovieTitle" mt ON m.id = mt."movieId"
+          WHERE mt.title % ${searchTitle} 
+          ORDER BY similarity(mt.title, ${searchTitle}) DESC
+        LIMIT ${queryLimit};`
+
+    return result
   }
 }
