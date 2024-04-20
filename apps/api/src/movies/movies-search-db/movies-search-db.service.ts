91fc7be9ffd742000ea62d68a51d6216b0d82e25
Mathias Private Laptop <mathiasberggren1@gmail.com>
2024-04-20 14:21:19 +0200

    fix(api): :bug: Correctly post-process the fuzzy search payload and create interfaces to handle it smoother

--- apps/api/src/movies/movies-search-db/movies-search-db.service.ts
+++ apps/api/src/movies/movies-search-db/movies-search-db.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common'
-import { Movie } from '@prisma/client'
+import { Movie, MovieTitle } from '@prisma/client'
 
 import { MoviesSearch } from '../interfaces/movies-search.interface'
 import { PrismaService } from '../../database/prisma.service'
+
+interface MovieWithMovieTitleRow {
+  id: number
+  genre: string
+  director: string
+  duration: number
+  subtitles: string[]
+  releaseDate: Date
+
+  movieTitleId: number
+  title: string
+  movieId: number
+  language: string
+}
+
+// TODO: Should be possible to derive from Prisma
+type MovieWithTitle = Movie & { movieTitles: MovieTitle[] }
 
 @Injectable()
 export class MoviesSearchDbService implements MoviesSearch {
@@ -10,14 +27,39 @@ export class MoviesSearchDbService implements MoviesSearch {
     private readonly db: PrismaService
   ) {}
 
-  async findByTitle (searchTitle: string, queryLimit: number = 10) {
-    const result = await this.db.$queryRaw<Movie[]>`
-      SELECT m.* FROM "Movie" m
-        JOIN "MovieTitle" mt ON m.id = mt."movieId"
-          WHERE mt.title % ${searchTitle} 
-          ORDER BY similarity(mt.title, ${searchTitle}) DESC
+  async findByTitle (searchTitle: string, queryLimit: number = 10): Promise<MovieWithTitle[]> {
+    const queryResult = await this.db.$queryRaw<MovieWithMovieTitleRow[]>`
+      SELECT m.*, t.* FROM "Movie" m
+        JOIN "MovieTitle" t ON m.id = mt."movieId"
+          WHERE t.title % ${searchTitle} 
+          ORDER BY similarity(t.title, ${searchTitle}) DESC
         LIMIT ${queryLimit};`
 
-    return result
+    const processedResult = queryResult.reduce<MovieWithTitle[]>((acc, row) => {
+      // Look for if we already processed the movie
+      const movie = acc.find(elem => elem.id === row.id)
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      if (!movie) {
+        const { id, genre, director, duration, subtitles, releaseDate, ...movieTitle } = row
+
+        acc.push({
+          id,
+          genre,
+          director,
+          duration,
+          subtitles,
+          releaseDate,
+          movieTitles: [movieTitle]
+        })
+        // if we already processed the movie, this is the same entry but with another title
+      } else {
+        const { id, genre, director, duration, subtitles, releaseDate, ...movieTitle } = row
+
+        movie.movieTitles.push(movieTitle)
+      }
+      return acc
+    }, [])
+
+    return processedResult
   }
 }
