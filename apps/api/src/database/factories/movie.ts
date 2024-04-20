247a7034489d3319c54a05dcb688d748be9ca431
Mathias Private Laptop <mathiasberggren1@gmail.com>
2024-04-20 16:46:17 +0200

    test(api): :technologist: Update the movie factory to not require movieTitles to be passed

--- apps/api/src/database/factories/movie.ts
+++ apps/api/src/database/factories/movie.ts
@@ -8,7 +8,7 @@ import { movieTitleFactory } from './movieTitle'
 import { generateMovieTitles } from './utils/generateMovieTitles'
 
 interface IMovieFactory extends Movie {
-  movieTitles: MovieTitle[]
+  movieTitles?: MovieTitle[]
 }
 
 class MovieFactory extends Factory<IMovieFactory> {}
@@ -20,18 +20,19 @@ export const movieFactory = MovieFactory.define(({ sequence, onCreate, params })
     const createdMovie = await prisma.movie.create({ data: movie })
 
     // I know this looks horrible, but thinking this is okay since it's just a factory.
+    const movieTitles = await Promise.all(
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    const movieTitles = await Promise.all(params.movieTitles
-      ? params.movieTitles.map(async movieTitle => await movieTitleFactory.create(movieTitle))
-      : Object.entries(generateMovieTitles()).map(async ([language, title]) => {
-        const movieTitleParams = {
-          movieId: movie.id,
-          language,
-          title
-        }
+      params.movieTitles
+        ? params.movieTitles.map(async movieTitle => await movieTitleFactory.create(movieTitle))
+        : Object.entries(generateMovieTitles()).map(async ([language, title]) => {
+          const movieTitleParams = {
+            movieId: movie.id,
+            language,
+            title
+          }
 
-        return await movieTitleFactory.create(movieTitleParams)
-      })
+          return await movieTitleFactory.create(movieTitleParams)
+        })
     )
 
     return {
@@ -46,7 +47,6 @@ export const movieFactory = MovieFactory.define(({ sequence, onCreate, params })
     director: faker.person.fullName(),
     duration: faker.number.int({ min: 60, max: 240 }),
     subtitles: faker.helpers.arrayElements(supportedLanguageLocaleCodes, faker.number.int({ min: 0, max: 4 })),
-    releaseDate: new Date(),
-    movieTitles: []
+    releaseDate: new Date()
   }
 })
