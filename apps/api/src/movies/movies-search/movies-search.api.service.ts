ee2e2c95f299e1e557a672670af1118e780674fb
rasouza <alves.wm@gmail.com>
2024-04-30 17:32:52 +0200

    refactor(api): refactor movie search API service to comply with new interface

--- apps/api/src/movies/movies-search/movies-search.api.service.ts
+++ apps/api/src/movies/movies-search/movies-search.api.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@nestjs/common'
 import { firstValueFrom } from 'rxjs'
 
-import { MoviesSearch } from '../interfaces/movies-search.interface'
 import { ImdbApiHttpService } from '../clients/imdb-api.service'
 import { IMDBMovie } from '../interfaces/imdb-api.interface'
+import { Movie } from '../entities/movie.entity'
+
+import { MoviesSearchService } from './movies-search.service'
 
 @Injectable()
-export class MoviesSearchApiService implements MoviesSearch {
-  constructor (private readonly imdbClient: ImdbApiHttpService) {}
+export class MoviesSearchApiService extends MoviesSearchService {
+  constructor (private readonly imdbClient: ImdbApiHttpService) {
+    super()
+  }
 
-  async findByTitle (title: string): Promise<IMDBMovie[]> {
-    const { data } = await firstValueFrom(this.imdbClient.get('/api/v1/searchIMDB', { params: { query: title } }))
-    const { data: movies } = data
-    return movies
+  async findByTitle (title: string): Promise<Movie[]> {
+    const { data: response } = await firstValueFrom(this.imdbClient.get('/api/v1/searchIMDB', { params: { query: title } }))
+    const movies: IMDBMovie[] = response.data
+
+    /**
+     * TODO: refactor IMDB API to return the rest of the data
+     * - genre
+     * - director
+     */
+    return movies.map(movie => ({
+      // Hash string into a number
+      id: movie.title.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0),
+      releaseDate: new Date(`${movie.year}-01-01`),
+      titles: [
+        {
+          id: movie.title.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0),
+          title: movie.title,
+          language: 'en',
+          createdAt: new Date(),
+          updatedAt: new Date()
+          // TODO: include poster in MovieTitle table
+          // image: movie.image
+        }
+      ],
+      genre: 'TBD',
+      director: 'TBD',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }))
   }
 }
