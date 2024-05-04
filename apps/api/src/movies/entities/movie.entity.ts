96766e927b4bfbe3a68991a5509975b7c0b361d3
rasouza <alves.wm@gmail.com>
2024-05-04 20:26:19 +0200

    refactor(api): add missing fields to Movie entitiy

--- apps/api/src/movies/entities/movie.entity.ts
+++ apps/api/src/movies/entities/movie.entity.ts
@@ -5,12 +5,14 @@ const MovieSchema = z.object({
   id: z.number(),
   genre: z.string(),
   director: z.string(),
+  duration: z.number(),
   subtitles: z.string().array().optional(),
   releaseDate: z.date(),
   movieTitles: z.array(z.object({
     id: z.number(),
     title: z.string(),
     language: z.string(),
+    image: z.string(),
     createdAt: z.date(),
     updatedAt: z.date()
   })),
